00ad5a3c67c74f490c2a86406372ccf6d1c66320
zxyuling <1275869735@qq.com>
2017-02-27 13:46:32 +0800

    l

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express2' });
+  res.render('index', { title: 'Express3' });
 });
 
 module.exports = router;
