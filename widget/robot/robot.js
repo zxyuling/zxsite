eafc3e2d5fb935edab8d21e88dd7674d9a08d7e5
zxyuling <1275869735@qq.com>
2017-05-08 11:03:34 +0800

    v

--- widget/robot/robot.js
+++ widget/robot/robot.js
@@ -3,4 +3,4 @@ var move = require('./lib/move');
 var audio = require('./lib/audio');
 var db = require('./lib/doBanner');
 move('#J-robot');
-audio.addEventLister('#J-audio',db);
+audio.addEventLister('#J-audio',{analyser:db.analyser,ended:db.ended});
